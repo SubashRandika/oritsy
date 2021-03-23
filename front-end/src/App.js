eb90e79dc8b0787c0c4dfa28cb4f1bea083d9c98
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-03-23 21:38:23 +0530

    Removed antd and added tailwind css with create react app configurations. Added initial layout with Header, Footer and Conatiner.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import 'antd/dist/antd.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
 	return (
 		<>
+			<Header />
 			<h1>Welcome to Oritsy Shop</h1>
+			<Footer />
 		</>
 	);
 };
