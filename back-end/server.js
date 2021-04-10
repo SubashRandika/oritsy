e1b9671507669ef92f516442dfdd2f5cdb562671
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-04-10 20:04:58 +0530

    Styled with info success and error coloring of log messages with colors js module.

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import connectDB from '../back-end/config/database.js';
 import products from '../back-end/data/products.js';
+
+colors.setTheme({
+	info: ['brightYellow', 'bold'],
+	success: ['brightCyan', 'bold', 'underline'],
+	error: ['brightRed', 'bold', 'underline']
+});
 
 dotenv.config();
 
@@ -29,6 +36,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
 	console.log(
-		`Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`
+		`Server listening in ${process.env.NODE_ENV} mode on port ${PORT}`.info
 	)
 );
