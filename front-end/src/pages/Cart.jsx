c505f054cf46c057f62e1b38e3ba0bde64970585
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-05-10 19:45:53 +0530

    Implemented cart subtotal with items count and proceed to checkout button with styles.

--- front-end/src/pages/Cart.jsx
+++ front-end/src/pages/Cart.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { ImCreditCard } from 'react-icons/im';
 import CartItem from '../components/CartItem';
 import { cartSelector } from '../redux/slices/cartSlice';
 
 const Cart = () => {
 	const { cartItems } = useSelector(cartSelector);
 
-	const getTotalCartItems = () => {};
+	const getTotalCartItems = () => {
+		const itemTotal = cartItems.reduce(
+			(currentQuantity, currentItem) => currentQuantity + currentItem.quantity,
+			0
+		);
 
-	const getTotalPrice = () => {};
+		if (itemTotal > 1) {
+			return `(${itemTotal} items)`;
+		} else {
+			return `(${itemTotal} item)`;
+		}
+	};
+
+	const getTotalPrice = () => {
+		return cartItems
+			.reduce(
+				(currentPrice, currentItem) => currentPrice + currentItem.price,
+				0
+			)
+			.toFixed(2);
+	};
 
 	return (
 		<main className='container m-auto py-4 h-full'>
@@ -24,12 +43,26 @@ const Cart = () => {
 					)}
 				</div>
 				<div>
-					<div>
-						<p>{`Subtotal (${getTotalCartItems()}) Items`}</p>
-						<p>{`Total Price: ${getTotalPrice()}`}</p>
-					</div>
-					<div>
-						<button>Go to checkout</button>
+					<div className='border border-gray-200 shadow-md'>
+						<div className='border-b border-gray-200 p-6'>
+							<p className='text-2xl font-bold'>
+								Cart Subtotal
+								<span className='font-normal text-gray-500 px-3'>
+									{getTotalCartItems()}
+								</span>
+							</p>
+							<p className='text-2xl font-bold mt-5 text-red-700'>
+								{`$ ${getTotalPrice()}`}
+							</p>
+						</div>
+						<div className='p-6 flex justify-center items-center'>
+							<button className='flex items-center bg-gradient-to-r from-yellow-400 via-yellow-500 to-red-400 text-white font-semibold px-6 py-2 shadow-md hover:shadow-lg transition duration-300 ease-in-out'>
+								<span className='mr-3 uppercase'>Proceed to checkout</span>
+								<span className='text-xl'>
+									<ImCreditCard />
+								</span>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
