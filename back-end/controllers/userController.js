00eb84642a38bc6585bb1a9199f399411b9a5c0b
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-05-11 21:21:14 +0530

    Implemented new user registraion route with password hash in model level. Fixed a bug in login route when incorrect password provided.

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -3,7 +3,7 @@ import generateToken from '../utils/generateToken.js';
 import User from '../models/User.js';
 
 /**
- * @desc    user authenticate and login
+ * @desc    User authenticate and login
  * @route   POST /api/users/login
  * @access  Public
  */
@@ -13,10 +13,10 @@ const userLogin = asyncHandler(async (req, res) => {
 
 	if (!user) {
 		res.status(401);
-		throw new Error('Invalid email or password provided');
+		throw new Error('Invalid email provided');
 	}
 
-	if (user.matchPassword(password)) {
+	if (await user.matchPassword(password)) {
 		const { _id, name, email, isAdmin } = user;
 
 		return res.status(200).json({
@@ -26,6 +26,45 @@ const userLogin = asyncHandler(async (req, res) => {
 			isAdmin,
 			token: generateToken(_id)
 		});
+	} else {
+		res.status(401);
+		throw new Error('Invalid password provided');
+	}
+});
+
+/**
+ * @desc    Register a new user
+ * @route   POST /api/users
+ * @access  Public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+	const existingUser = await User.findOne({ email });
+
+	if (existingUser) {
+		res.status(400);
+		throw new Error('User already registered with this email');
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password
+	});
+
+	if (user) {
+		const { _id, name, email, isAdmin } = user;
+
+		res.status(201).json({
+			_id,
+			name,
+			email,
+			isAdmin,
+			token: generateToken(_id)
+		});
+	} else {
+		res.status(400);
+		throw new Error('Incorrect user details provided');
 	}
 });
 
@@ -52,4 +91,4 @@ const getUserProfile = async (req, res) => {
 	});
 };
 
-export { userLogin, getUserProfile };
+export { userLogin, registerUser, getUserProfile };
--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -1,10 +1,15 @@
 import express from 'express';
 const router = express.Router();
-import { userLogin, getUserProfile } from '../controllers/userController.js';
+import {
+	userLogin,
+	registerUser,
+	getUserProfile
+} from '../controllers/userController.js';
 import secure from '../middlewares/auth.js';
 
 // all auth and user related routes
-router.post('/login', userLogin);
+router.route('/').post(registerUser);
+router.route('/login').post(userLogin);
 router.route('/profile').get(secure, getUserProfile);
 
 export default router;
