7711faf6a41209db4aea25a4defb941f1a81c673
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-05-30 23:50:24 +0530

    Implemented backend controllers and routes for both create and update product endpoints.

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -31,7 +31,7 @@ const getProductById = asyncHandler(async (req, res) => {
 /**
  * @desc    Delete a single product by id. Only for admin.
  * @route   DELETE /api/products/:id
- * @access  Public/Admin
+ * @access  Private/Admin
  */
 const deleteProduct = asyncHandler(async (req, res) => {
 	const product = await Product.findById(req.params.id);
@@ -46,4 +46,61 @@ const deleteProduct = asyncHandler(async (req, res) => {
 	res.status(200).json({ message: 'Product was successfully deleted' });
 });
 
-export { getAllProducts, getProductById, deleteProduct };
+/**
+ * @desc    Create a dummy product. Only for admin.
+ * @route   POST /api/products
+ * @access  Private/Admin
+ */
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Dummy Product',
+		description: 'Dummy Product description',
+		image:
+			'https://www.gemkom.com.tr/wp-content/uploads/2020/02/NO_IMG_600x600-1.png',
+		brand: 'Dummy brand',
+		price: 0,
+		category: 'Dummy category',
+		countInStock: 0,
+		numReviews: 0,
+		user: req.user._id
+	});
+
+	const createdProduct = await product.save();
+	res.status(201).json(createdProduct);
+});
+
+/**
+ * @desc    Update existing product. Only for admin.
+ * @route   PUT /api/products/:id
+ * @access  Private/Admin
+ */
+const updateProduct = asyncHandler(async (req, res) => {
+	const { name, description, image, price, brand, category, countInStock } =
+		req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (!product) {
+		res.status(404);
+		throw new Error('Product cannot be found');
+	}
+
+	product.name = name;
+	product.description = description;
+	product.image = image;
+	product.price = price;
+	product.brand = brand;
+	product.category = category;
+	product.countInStock = countInStock;
+
+	const updateProduct = await product.save();
+	res.status(200).json(updateProduct);
+});
+
+export {
+	getAllProducts,
+	getProductById,
+	deleteProduct,
+	createProduct,
+	updateProduct
+};
--- back-end/routes/product.js
+++ back-end/routes/product.js
@@ -3,14 +3,17 @@ const router = express.Router();
 import {
 	getAllProducts,
 	getProductById,
-	deleteProduct
+	deleteProduct,
+	createProduct,
+	updateProduct
 } from '../controllers/productController.js';
 import { secure, adminOnly } from '../middlewares/auth.js';
 
 // all products related routes
-router.route('/').get(getAllProducts);
+router.route('/').get(getAllProducts).post(secure, adminOnly, createProduct);
 router
 	.route('/:id')
+	.put(secure, adminOnly, updateProduct)
 	.get(getProductById)
 	.delete(secure, adminOnly, deleteProduct);
 
