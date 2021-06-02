a5f016dd4ccc343aecf339cb0ea5cdeb89d7f9de
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-06-02 22:23:19 +0530

    Implemented place order screen loading spinner until paypal buttons loaded to screen.

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
-import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+import {
+	PayPalScriptProvider,
+	PayPalButtons,
+	usePayPalScriptReducer
+} from '@paypal/react-paypal-js';
 import { toast } from 'react-toastify';
 import dayjs from 'dayjs';
 import { getOrderDetails, payOrder } from '../redux/actions/orderActions';
@@ -23,6 +27,7 @@ const OrderDetails = () => {
 	const dispatch = useDispatch();
 	const { order, loading } = useSelector(orderDetailsSelector);
 	const { paySuccess } = useSelector(orderPaymentSelector);
+	const [{ isPending }] = usePayPalScriptReducer();
 
 	const withDecimal = (value) => {
 		return (Math.round(value * 100) / 100).toFixed(2);
@@ -240,11 +245,36 @@ const OrderDetails = () => {
 								{!order.isPaid && (
 									<div className='p-6 w-full border-t border-gray-300'>
 										<PayPalScriptProvider options={{ 'client-id': clientId }}>
-											<PayPalButtons
-												createOrder={handleOrderCreation}
-												onApprove={handlePaymentSuccess}
-												onError={handlePaymentFailed}
-											/>
+											{isPending ? (
+												<div className='w-full flex items-center justify-center'>
+													<svg
+														className='animate-spin -ml-1 mr-3 h-8 w-8 text-gray-600'
+														xmlns='http://www.w3.org/2000/svg'
+														fill='none'
+														viewBox='0 0 24 24'
+													>
+														<circle
+															className='opacity-25'
+															cx='12'
+															cy='12'
+															r='10'
+															stroke='currentColor'
+															strokeWidth='4'
+														></circle>
+														<path
+															className='opacity-75'
+															fill='currentColor'
+															d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+														></path>
+													</svg>
+												</div>
+											) : (
+												<PayPalButtons
+													createOrder={handleOrderCreation}
+													onApprove={handlePaymentSuccess}
+													onError={handlePaymentFailed}
+												/>
+											)}
 										</PayPalScriptProvider>
 									</div>
 								)}
